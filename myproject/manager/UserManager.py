dab3f1349ca917551a64e645b2ca4a7af6fd1f6a
Sasha <sasha.azad@gatech.edu>
2016-11-17 13:44:00 -0500

    Django Tutorial v1.0 - Includes accompanying presentation in .keynote, .pptx, and .pdf format - Includes simple code project

--- myproject/manager/UserManager.py
+++ myproject/manager/UserManager.py
@@ -17,17 +17,17 @@ def userRequest(request, user_id=None):
 def createUser(request):
 	first_name = request.POST.get('first_name','')
 	last_name = request.POST.get('last_name','')
-	email = request.POST.get('email','')
+	username = request.POST.get('username','')
 
 	user = None
-	existing_users = DPUser.objects.filter(email=email)
+	existing_users = DPUser.objects.filter(username=username)
 
-	print first_name, last_name, email
+	print first_name, last_name, username
 	print len(existing_users)
 	if len(existing_users) > 0:
-		# User Exists!
+		# Player Exists!
 		user = existing_users[0]
-		errorMessage = "Error! User with this email already exists."
+		errorMessage = "Error! Player with this username already exists."
 
 		return HttpResponse(json.dumps({'success': False, "error":errorMessage}), content_type="application/json")
 
@@ -37,7 +37,7 @@ def createUser(request):
 	user.first_name = first_name
 	user.last_name = last_name
 	user.email = email
-	
+
 	user.save()
 
 	response_data = user.getResponseData()
@@ -51,7 +51,6 @@ def getUser(request, user_id):
 	
 	if user_id:
 		users = DPUser.objects.filter(id=user_id)
-		
 
 		if len(users)>0:
 			user = users[0]
